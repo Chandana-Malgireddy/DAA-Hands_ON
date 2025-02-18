31f7e753bee24af6c394242b428f6f11a02276dc
Chandana Malgireddy <malgireddychandana99@gmail.com>
2025-02-17 20:41:18 -0600

    Hands_On5

---
+++ Hands_On5/MinHeap.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_On5
+{
+    public class MinHeap<T> where T : IComparable<T>
+    {
+        private List<T> heap;
+        public MinHeap()
+        {
+            heap = new List<T>();
+        }
+        private int Parent(int i)
+        {
+            return (i - 1) >> 1; 
+        }
+        private int Left(int i)
+        {
+            return (i << 1) + 1; 
+        }
+        private int Right(int i)
+        {
+            return (i << 1) + 2; 
+        }
+        public void BuildMinHeap(List<T> data)
+        {
+            heap = data;
+            for (int i = heap.Count / 2 - 1; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+        private void Heapify(int i)
+        {
+            int left = Left(i);
+            int right = Right(i);
+            int smallest = i;           
+            if (left < heap.Count && heap[left].CompareTo(heap[smallest]) < 0)
+            {
+                smallest = left;
+            }
+            if (right < heap.Count && heap[right].CompareTo(heap[smallest]) < 0)
+            {
+                smallest = right;
+            }
+            if (smallest != i)
+            {
+                Swap(i, smallest);
+                Heapify(smallest);
+            }
+        }
+        public T PopRoot()
+        {
+            if (heap.Count == 0)
+                throw new InvalidOperationException("Heap is empty");
+
+            T root = heap[0];
+            T lastElement = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+
+            if (heap.Count > 0)
+            {
+                heap[0] = lastElement;
+                Heapify(0);
+            }
+
+            return root;
+        }
+        public void Push(T value)
+        {
+            heap.Add(value);
+            BubbleUp(heap.Count - 1);
+        }
+        private void BubbleUp(int i)
+        {
+            while (i > 0 && heap[Parent(i)].CompareTo(heap[i]) > 0)
+            {
+                Swap(i, Parent(i));
+                i = Parent(i);
+            }
+        }
+        private void Swap(int i, int j)
+        {
+            T temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+        public T Peek()
+        {
+            if (heap.Count == 0)
+                throw new InvalidOperationException("Heap is empty");
+
+            return heap[0];
+        }
+        public int Size()
+        {
+            return heap.Count;
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", heap);
+        }
+    }
+}
---
+++ Hands_On5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Hands_On5;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Example 1: MinHeap with integers
+        MinHeap<int> minHeap = new MinHeap<int>();
+        minHeap.BuildMinHeap(new List<int> { 5, 13, 2, 25, 7, 17, 20, 8, 4 });
+        Console.WriteLine("Initial Min Heap: " + minHeap);
+
+        // Pop the root
+        Console.WriteLine("Popped root (min): " + minHeap.PopRoot());
+        Console.WriteLine("Min Heap after pop: " + minHeap);
+
+        // Push a new element
+        minHeap.Push(1);
+        Console.WriteLine("Min Heap after push 1: " + minHeap);
+
+        // Peek at the root
+        Console.WriteLine("Peek root: " + minHeap.Peek());
+
+        // Pop the root again
+        Console.WriteLine("Popped root (min): " + minHeap.PopRoot());
+        Console.WriteLine("Min Heap after second pop: " + minHeap);
+
+        // Example 2: MinHeap with floating point numbers
+        MinHeap<double> minHeap2 = new MinHeap<double>();
+        minHeap2.BuildMinHeap(new List<double> { 3.14, 1.59, 2.65, 5.89, 7.32 });
+        Console.WriteLine("Initial Min Heap with floats: " + minHeap2);
+        minHeap2.Push(1.41);
+        Console.WriteLine("Min Heap after push 1.41: " + minHeap2);
+        Console.WriteLine("Popped root from float heap: " + minHeap2.PopRoot());
+        Console.WriteLine("Min Heap after popping root: " + minHeap2);
+    }
+}
